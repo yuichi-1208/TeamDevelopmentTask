20f7cf47b927175b856f6f6a896af9a9db1b37c7
yuichi oyama <yuichi19991208@gmail.com>
2020-10-27 23:18:09 +0900

    agendaの削除機能を作成

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,11 +15,22 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
   end
+
+  def destroy
+    @agenda = Agenda.find(params[:id])
+    if @agenda.user_id == current_user.id || @agenda.team.owner_id == current_user.id
+      @agenda.destroy
+      redirect_to dashboard_path, notice: "#{@agenda.title}を削除しました"
+    else
+      redirect_to dashboard_path, notice: "削除できません"
+    end
+  end
+
 
   private
 
